66a6a5f086b7971eb939c706fa0630f482763482
Explodisemquerer <gustavohenriquesiqueira10@gmail.com>
2022-06-07 23:57:01 -0300

    primeiro teste - mocha

---
+++ mocha-chai/test/sample.spec.js
@@ -0,0 +1,22 @@
+const { assert } = require("chai");
+
+describe('primeiro teste', () =>{
+    it('verificando igualdade', () => {
+        let numero = 25; 
+        assert.strictEqual(numero,25)
+    })
+    it('teste 2', () => {
+        let numero = 25; 
+        assert.strictEqual(numero,25)
+})
+})
+describe('segundo teste', () =>{
+    it('verificando desigualdade', () => {
+        let numero = 25; 
+        assert.notDeepEqual(numero,'25')
+    })
+    it('teste 2', () => {
+        let numero = 'vaca'; 
+        assert.notEqual(numero,'luva')
+})
+})
